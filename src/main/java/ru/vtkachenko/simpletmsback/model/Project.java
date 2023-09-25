2da6e1e46c677056464f0002ba1edbd28a562ae7
Vitaliy Tkachenko <tkachenko.java@yandex.ru>
2023-09-25 18:54:50 +0500

    start create project entity

--- src/main/java/ru/vtkachenko/simpletmsback/model/Project.java
+++ src/main/java/ru/vtkachenko/simpletmsback/model/Project.java
@@ -1,4 +1,31 @@
 package ru.vtkachenko.simpletmsback.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@Table(name = "projects")
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    @CreationTimestamp
+    private Instant createdDt;
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private Instant modifiedDt;
+
+    private String name;
+    private String description;
 }
