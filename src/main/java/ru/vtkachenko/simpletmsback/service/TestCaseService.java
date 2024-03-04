a749ca0885098f1666344f3a9d4f52ae3599bd2f
Tkachenko Vitaliy <tkachenko.java@yandex.ru>
2024-03-04 08:32:37 +0500

    Просто сохранение

--- src/main/java/ru/vtkachenko/simpletmsback/service/TestCaseService.java
+++ src/main/java/ru/vtkachenko/simpletmsback/service/TestCaseService.java
@@ -1,22 +1,29 @@
 package ru.vtkachenko.simpletmsback.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.vtkachenko.simpletmsback.exception.business.ProjectNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+import ru.vtkachenko.simpletmsback.dto.TestCaseDto;
+import ru.vtkachenko.simpletmsback.dto.TestCaseStepDto;
+import ru.vtkachenko.simpletmsback.dto.TestStepDto;
 import ru.vtkachenko.simpletmsback.exception.business.TestCaseNotFoundException;
+import ru.vtkachenko.simpletmsback.mapper.TestCaseMapper;
+import ru.vtkachenko.simpletmsback.model.StepCaseRel;
 import ru.vtkachenko.simpletmsback.model.TestCase;
 import ru.vtkachenko.simpletmsback.repository.TestCaseRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TestCaseService {
     private final TestCaseRepository testCaseRepository;
+    private final TestCaseMapper mapper;
+    private final TestStepService testStepService;
 
-    @Autowired
-    public TestCaseService(TestCaseRepository testCaseRepository) {
-        this.testCaseRepository = testCaseRepository;
-    }
 
     public TestCase getTestCase(Long id) {
         return testCaseRepository.getTestCaseById(id).orElseThrow(() -> {
@@ -24,5 +31,26 @@ public class TestCaseService {
             log.error(message);
             throw new TestCaseNotFoundException(message);
         });
+    }
+
+    @Transactional
+    public TestCaseDto createTestCase(TestCaseDto testCaseDto) {
+        TestCase testCase = mapper.toEntity(testCaseDto);
+        TestCase savedTestCase = testCaseRepository.save(testCase);
+        List<TestStepDto> testStepDtos = testCaseDto.getSteps().stream()
+                .map(TestCaseStepDto::getTestStep)
+                .collect(Collectors.toList());
+        List<TestStepDto> saveTestSteps = testStepService.saveAllTestSteps(testStepDtos);
+
+//        testCaseDto.getSteps().stream()
+//                .map(testCaseStepDto -> {
+//                    StepCaseRel.builder()
+//                            .orderNumber(testCaseStepDto.getOrderNumber())
+//                            .testCase(savedTestCase)
+//                            .testStep()
+//                            .build();
+//                })
+
+        return null;
     }
 }
--- src/main/java/ru/vtkachenko/simpletmsback/service/TestStepService.java
+++ src/main/java/ru/vtkachenko/simpletmsback/service/TestStepService.java
@@ -1,22 +1,38 @@
 package ru.vtkachenko.simpletmsback.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.vtkachenko.simpletmsback.dto.TestStepDto;
+import ru.vtkachenko.simpletmsback.mapper.TestStepMapper;
 import ru.vtkachenko.simpletmsback.model.TestStep;
 import ru.vtkachenko.simpletmsback.repository.TestStepRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-@Slf4j
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class TestStepService {
 
     private final TestStepRepository testStepRepository;
+    private final TestStepMapper mapper;
 
-    @Autowired
-    public TestStepService(TestStepRepository testStepRepository) {
-        this.testStepRepository = testStepRepository;
+    public TestStepDto createTestStep(TestStepDto testStepDto) {
+        return null;
+    }
+
+    public List<TestStepDto> saveAllTestSteps(List<TestStepDto> testStepDtos) {
+        List<TestStep> testSteps = testStepDtos.stream()
+                .map(mapper::toEntity)
+                .collect(Collectors.toList());
+        List<TestStep> savedTestSteps = testStepRepository.saveAll(testSteps);
+        return savedTestSteps.stream()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
     }
 
 }
