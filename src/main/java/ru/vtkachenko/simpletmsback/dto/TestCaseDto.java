855f4c7e2ffdec4eae9656bd497666b2e12d75f0
Tkachenko Vitaliy <tkachenko.java@yandex.ru>
2024-03-13 08:17:11 +0500

    Исправление dto

--- src/main/java/ru/vtkachenko/simpletmsback/dto/TestCaseDto.java
+++ src/main/java/ru/vtkachenko/simpletmsback/dto/TestCaseDto.java
@@ -1,5 +1,6 @@
 package ru.vtkachenko.simpletmsback.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ public class TestCaseDto {
     @NotNull
     private String name;
     private String preconditions;
+    @JsonProperty("testSteps")
     @Builder.Default
     private List<TestCaseStepDto> steps = new ArrayList<>();
     @NotNull
