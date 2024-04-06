ee6f35a13f3e3fc01992e578ac2a9475ef354ee1
Tkachenko Vitaliy <tkachenko.java@yandex.ru>
2024-04-06 21:54:09 +0500

    next step

--- src/main/java/ru/vtkachenko/simpletmsback/dto/TestPlanDto.java
+++ src/main/java/ru/vtkachenko/simpletmsback/dto/TestPlanDto.java
@@ -14,6 +14,8 @@ public class TestPlanDto {
     private Long id;
     @NotNull
     private Long projectId;
+    @NotNull
+    private String name;
     private String description;
     @Builder.Default
     private Set<TestCaseShortDto> testCases = new HashSet<>();
---
+++ src/main/java/ru/vtkachenko/simpletmsback/mapper/TestPlanMapper.java
@@ -0,0 +1,42 @@
+package ru.vtkachenko.simpletmsback.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.vtkachenko.simpletmsback.dto.TestPlanDto;
+import ru.vtkachenko.simpletmsback.dto.response.TestCaseShortDto;
+import ru.vtkachenko.simpletmsback.model.TestPlan;
+import ru.vtkachenko.simpletmsback.service.ProjectService;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class TestPlanMapper implements EntityMapper<TestPlanDto, TestPlan> {
+    private final ProjectService projectService;
+    private final TestCaseShortMapper testCaseShortMapper;
+    @Override
+    public TestPlanDto toDto(TestPlan entity) {
+        Set<TestCaseShortDto> testCasesShortDto = entity.getTestCases().stream()
+                .map(testCaseShortMapper::toDto)
+                .collect(Collectors.toSet());
+
+        return TestPlanDto.builder()
+                .id(entity.getId())
+                .projectId(entity.getProject().getId())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .testCases(testCasesShortDto)
+                .build();
+    }
+
+    @Override
+    public TestPlan toEntity(TestPlanDto dto) {
+        return TestPlan.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .project(projectService.getProjectReferenceById(dto.getProjectId()))
+                .build();
+    }
+}
