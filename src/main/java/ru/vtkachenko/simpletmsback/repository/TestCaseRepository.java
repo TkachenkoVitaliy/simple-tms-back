cf21f3a0bbc983ffcc7a77f45763854378f1abf6
Tkachenko Vitaliy <tkachenko.java@yandex.ru>
2024-04-08 10:09:57 +0500

    cleanup imports

--- src/main/java/ru/vtkachenko/simpletmsback/repository/TestCaseRepository.java
+++ src/main/java/ru/vtkachenko/simpletmsback/repository/TestCaseRepository.java
@@ -3,7 +3,6 @@ package ru.vtkachenko.simpletmsback.repository;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.vtkachenko.simpletmsback.model.Project;
 import ru.vtkachenko.simpletmsback.model.TestCase;
 
 import java.util.List;
@@ -13,9 +12,6 @@ public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
 
     List<TestCase> findTestCaseByProject_Id(Long projectId);
     Optional<TestCase> findById(@NotNull Long testCaseId);
-
-//    @EntityGraph(attributePaths = {"testSteps", "testSteps.testStep"}, type = EntityGraph.EntityGraphType.LOAD)
-//    Optional<TestCase> getTestCaseById(@NotNull Long testCaseId);
 
     @EntityGraph(value = "TestCase.testSteps.testStep", type = EntityGraph.EntityGraphType.LOAD)
     Optional<TestCase> getTestCaseById(@NotNull Long testCaseId);
