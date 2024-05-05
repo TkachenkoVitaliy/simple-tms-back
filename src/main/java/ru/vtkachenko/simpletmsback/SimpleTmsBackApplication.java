2a922942ca2599764d602d36d3258bfefb6031bf
Tkachenko Vitaliy <tkachenko.java@yandex.ru>
2024-05-05 11:50:20 +0500

    add mongodb

--- src/main/java/ru/vtkachenko/simpletmsback/SimpleTmsBackApplication.java
+++ src/main/java/ru/vtkachenko/simpletmsback/SimpleTmsBackApplication.java
@@ -2,8 +2,10 @@ package ru.vtkachenko.simpletmsback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SimpleTmsBackApplication {
 
 	public static void main(String[] args) {
